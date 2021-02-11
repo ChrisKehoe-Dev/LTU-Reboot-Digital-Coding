aa54d80231192515c979b040be70d83ff9dc640b
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-11 22:16:11 +0000

    Update task-3.1.6.js
    Take home task.
    
    Started with dummy value of 35.45, used tax @10%, calculated back to start to get net amount, then wrote to screen

---
+++ module-3/lesson-1/scripts/task-3.1.6.js
@@ -0,0 +1,15 @@
+var $mealTotal = 35.45
+var $taxPerct = 10
+var $taxCalc = (100 + $taxPerct) / 100
+var $mealNet = $mealTotal / $taxCalc
+$mealNet = $mealNet.toFixed(2) 
+var $taxAmount = $mealTotal - $mealNet
+$taxAmount = $taxAmount.toFixed(2) 
+var $TAB = "\t";
+
+//alert($mealNet)
+
+document.write("<p>Food and drink total £" + $mealNet + "</p>")
+document.write("<p>Tax @ " + $taxPerct + "% £" + $taxAmount + "</p>")
+document.write("<p><strong>Total including Tax £" + $mealTotal + "<strong></p>")
+
