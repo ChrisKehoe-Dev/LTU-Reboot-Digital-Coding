8a9cef1555d6c4d66137ca820f53fff8e4cd998e
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-11 20:10:55 +0000

    Update task-3.1.2.js
    Added comments
    Added alert, console.log and document.write as demonstration

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,13 @@
+/*
+This is a multiline comment
+Replicates as seen in SQL
+*/
+
+//This is a single line comment
+
+alert('Hello World, this is an alert (but the world is not ending'); //this is a pop up message
+console.log('Hello there in the console')//console logging message
+document.write('Hello, this is document.write writing to the document')//this writes to the screen
+
+
+
