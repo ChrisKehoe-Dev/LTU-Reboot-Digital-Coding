032a069f51cdf9038272ef8a77851a6d5710bcb3
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-11 20:51:45 +0000

    Update task-3.1.4.js
    declared 3 variables, 2 numeric one calc
    printed the numbers via alerts with additional text

---
+++ module-3/lesson-1/scripts/task-3.1.4.js
@@ -0,0 +1,9 @@
+var $numberOfKittensNow = 5
+var $numberOfKittensWanted = 10
+var $numberOfKittensReq = $numberOfKittensWanted -  $numberOfKittensNow 
+
+alert('I have ' + $numberOfKittensNow + ' kittens');
+
+alert('I want ' + $numberOfKittensWanted + ' kittens');
+
+alert('I need to buy ' + $numberOfKittensReq + ' kittens');
