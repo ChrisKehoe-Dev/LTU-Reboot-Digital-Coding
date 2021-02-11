4fda44b35bcd6b62351386e643a04d02945e292e
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-11 20:32:09 +0000

    Update task-3.1.3.js
    Variables
    
    declared variable $numberOfKittens
    
    alter console.loga nd document.write using it.
    
    messing with calculations, calc one does not work

---
+++ module-3/lesson-1/scripts/task-3.1.3.js
@@ -0,0 +1,18 @@
+/*
+messing
+var $numberOfKittens = 5
+
+alert('I have ' + $numberOfKittens + ' kittens');
+
+alert('i would likt to have 10 kittens');
+
+alert('i need to buy ' + 10 - $numberOfKittens + 'kittens');
+*/
+
+var $numberOfKittens = 5
+
+alert('I have ' + $numberOfKittens + ' kittens');
+
+console.log($numberOfKittens)
+
+document.write($numberOfKittens)
