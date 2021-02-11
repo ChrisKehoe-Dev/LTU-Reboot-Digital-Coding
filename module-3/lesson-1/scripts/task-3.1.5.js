116dd2cbd741d926fd7599754c8d5a3d49daed24
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-11 21:08:39 +0000

    Update task-3.1.5.js
    var - names and concatenation
    
    alert to screen to view
    
    2 diff methods of concatenation step by step and +=

---
+++ module-3/lesson-1/scripts/task-3.1.5.js
@@ -0,0 +1,12 @@
+var $fName = 'Chris';
+var $lName = 'Kehoe';
+var $fullName = $fName
+$fullName = $fullName + ' ' + $lName;
+
+alert($fullName);
+
+
+var $fullName2 = 'Chris '
+$fullName2 += 'Kehoe';
+
+alert($fullName2);
