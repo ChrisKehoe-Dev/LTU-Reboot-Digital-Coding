9dd3f00b7fb4d6e4dad93ceaff67da93c7d94858
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-16 20:18:59 +0000

    Update task-3.2.4.js
    if statement

---
+++ module-3/lesson-2/scripts/task-3.2.4.js
@@ -0,0 +1,12 @@
+var temp = 110
+var fCast 
+
+if(temp > 50) {
+     fCast = "Shorts and T-Shirts"
+} else if(temp < 0) {
+    fCast = "big coat"
+} else {
+    fCast =  "warm clothes"
+}
+
+document.write(fCast)
