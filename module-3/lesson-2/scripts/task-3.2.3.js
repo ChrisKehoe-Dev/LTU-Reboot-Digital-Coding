a989f8b0afab49793ee517e3b4cf3d321d8c8cdc
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-16 19:53:12 +0000

    Update task-3.2.3.js
    Added retrun to function, nested some variables, and document write to screen

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -1,14 +1,34 @@
 /* EXAMPLE */
-
+/*
 function greet(greeting, name) {
   return greeting + ' ' + name;
 }
 
 var message = greet('Hello', 'Luke');
     console.log( message ); // Output: Hello Luke
-
+*/
 /* EXAMPLE */
 
 // ADD YOUR CODE BELOW HERE
 
 // ADD YOUR CODE ABOVE HERE
+
+
+function fullName(firstName, lastName) {
+  var fName = firstName + " " + lastName
+  //document.write("<p>My full name is " + fName + ".</p>")
+  return fName
+}
+
+var firstNm = "Bernard";
+var lastNm = "McGinty";
+
+var mR = fullName (firstNm,lastNm) 
+
+var firstNm = "Alison";
+var lastNm = "Alexander";
+var mRs = fullName (firstNm,lastNm) 
+
+
+
+document.write("<p>My full name is " + mR + " and I am married to " + mRs + ".</p>")
