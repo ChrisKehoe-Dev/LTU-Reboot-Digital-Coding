986dc664e066ca521d20905118b25281424244c0
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-16 19:39:22 +0000

    Update task-3.2.2.js
    create basic function to concatenate first and last names and doc write to page

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,9 @@
+function fullName(firstName, lastName) {
+    var fName = firstName + " " + lastName
+    document.write("<p>My full name is " + fName + ".</p>")
+}
+
+var firstNm = "Bernard";
+var lastNm = "McGinty";
+
+fullName (firstNm,lastNm);
