7df5c19ed68101776806a4529bdcf714c54183fd
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-18 19:23:39 +0000

    Update task-3.3.1.js
    updated with variable use in the function

--- module-3/lesson-3/scripts/task-3.3.1.js
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -1,5 +1,11 @@
-function percentageCalculator(number, percentage) {
+/*function percentageCalculator(number, percentage) {
     return number * (percentage / 100)
-}
+}*/
 
-console.log (percentageCalculator(135 , 30) )
+//alternate - use variable in the function
+
+function percentageCalculator(number, percentage) {
+    var perctOf = (number * (percentage / 100))
+    return perctOf
+}
+console.log (percentageCalculator(135 , 25.4) )
