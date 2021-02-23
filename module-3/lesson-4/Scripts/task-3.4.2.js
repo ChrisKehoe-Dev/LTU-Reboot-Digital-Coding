6af84edd80f6c427402a2d8c00085ece08b0069b
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-23 19:59:15 +0000

    3.4.1
    html linked to js file.
    js created and array created

---
+++ module-3/lesson-4/Scripts/task-3.4.2.js
@@ -0,0 +1,5 @@
+let favouriteFoods = ['apples','pizza','bananas','curry','burgers','freshly baked bread']
+
+//array name then index number in square brackets to pick one
+
+console.log(favouriteFoods[2])
