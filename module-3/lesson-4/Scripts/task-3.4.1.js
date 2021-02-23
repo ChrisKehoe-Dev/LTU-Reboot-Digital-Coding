0ee6b2363429eb288b4f398d3b29d980adad14bf
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-23 19:38:57 +0000

    3.4.1
    html - add link to java script
    js. created folders, script and for loop
    1. basic
    2 loop through all times tables - 1-12
    follow up - format nicely in boxes

---
+++ module-3/lesson-4/Scripts/task-3.4.1.js
@@ -0,0 +1,15 @@
+//base
+for ( var j = 1; j <= 12; j++) {
+    document.write( 7 * i )
+}
+
+//challenge
+for ( var i = 1; i <= 12; i++) {
+        document.write( i + ' times tables')
+    for ( var j = 1; j <= 12; j++) {
+        document.write( '<p>' +  i + ' times ' + j + ' = ' + i * j + '</p>' )
+    }
+}
+
+// further challenge -  boxes round each and make them inline
+
