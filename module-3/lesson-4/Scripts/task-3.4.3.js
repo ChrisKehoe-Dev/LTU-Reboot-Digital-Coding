aeb17bbac52e7c0b5a594554fa088c71f00a3137
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-02-23 20:13:00 +0000

    3.4.3
    html - added link to js file
    js file - created, array created, loop with array length

---
+++ module-3/lesson-4/Scripts/task-3.4.3.js
@@ -0,0 +1,6 @@
+let favouriteFoods = ['apples','pizza','bananas','curry','burgers','freshly baked bread']
+
+
+for ( let arIndex = 0; arIndex < favouriteFoods.length; arIndex++ ) {
+    console.log(favouriteFoods[arIndex])
+}
