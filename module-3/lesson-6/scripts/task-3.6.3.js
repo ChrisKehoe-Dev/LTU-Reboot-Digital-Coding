de2205226b8b58d63c0a041a4facddfeb9bf2504
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-03-02 20:35:01 +0000

    task 3.6.3
    linked js and html
    created a mouse button
    used event listeners to create actions and alerts

---
+++ module-3/lesson-6/scripts/task-3.6.3.js
@@ -0,0 +1,9 @@
+let button = document.querySelector('button')
+
+/*button.addEventListener('mouseover' , function(){
+    alert("Hi")
+})
+*/
+button.addEventListener('mouseout' , function(){
+    alert("You clicked me")
+})
