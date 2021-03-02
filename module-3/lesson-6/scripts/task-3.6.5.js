49b1319d7b9b55f4013d06db17a107bae982552b
ChrisKehoe-Dev <77465746+ChrisKehoe-Dev@users.noreply.github.com>
2021-03-02 21:12:21 +0000

    task 3.6.5
    added a button to html and linked to js.
    alert event created to message out the input.

---
+++ module-3/lesson-6/scripts/task-3.6.5.js
@@ -0,0 +1,7 @@
+let fName = document.querySelector('input[name="full-name"]');
+
+
+let button = document.querySelector('button');
+    button.addEventListener('click', function(event) {
+        alert(fName.value)
+    }) ;
